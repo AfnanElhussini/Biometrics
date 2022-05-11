8856c6428a497358976df2725a3f0a0fc597f110
Afnan Elhussini <63553675+AfnanElhussini@users.noreply.github.com>
2022-05-11 04:03:04 +0200

    Add files via upload

---
+++ SEC9/burteforce.py
@@ -0,0 +1,16 @@
+import cv2
+img1 = cv2.imread('fingerwithnoise.png')
+img2 = cv2.cv2.imread('fingerprint.jpg')
+# ORB
+orb = cv2.ORB_create()
+kp1, des1 = orb.detectAndCompute(img1, None)
+kp2, des2 = orb.detectAndCompute(img2, None)
+bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck= True)
+matches = bf.match(des1, des2)
+matches= sorted(matches, key= lambda x:x.distance)
+result = cv2.drawMatches(img1, kp1,img2, kp2, matches, None)
+cv2.imshow("img1", img1)
+cv2.imshow("img2", img2)
+cv2.imshow("result", result)
+cv2.waitKey()
+cv2.destroyAllWindows()
